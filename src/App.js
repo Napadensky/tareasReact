443726145e95dbacdfdeb5568f465a4ee0ad563d
David Napadensky <davidnapadensky@gmail.com>
2020-07-03 19:43:36 -0300

    tarea1

--- src/App.js
+++ src/App.js
@@ -7,12 +7,22 @@ function App() {
     <div className="cont">
       <div className="App">
 
-       {/* Empezar condigo aqui  */}
+        <   Verde    />
+        <   Azul     />
+        <   Rojo     />
+        <   Amarillo />
+        <   Morado   />
 
       </div>
     </div>
   );
 }
 
+
+const Verde    = () => <div className="bg-green">     <h2>Verde</h2>       </div>;
+const Azul     = () => <div className="bg-blue">      <h2>Azul</h2>        </div>;
+const Rojo     = () => <div className="bg-red">       <h2>Rojo</h2>        </div>;
+const Amarillo = () => <div className="bg-yell">      <h2>Amarillo</h2>    </div>;
+const Morado   = () => <div className="bg-pur">       <h2>Morado</h2>      </div>;
 
 export default App;
