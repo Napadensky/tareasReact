a4ddb7a486f68303212a3668168cc118209a1852
David Napadensky <davidnapadensky@gmail.com>
2020-07-03 19:37:07 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="cont">
+      <div className="App">
+
+       {/* Empezar condigo aqui  */}
+
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
